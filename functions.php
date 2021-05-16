077becc8c9d5d382f05afb5f12f0c32dfe8642e3
Vanessa Rusu <vanessa@Vanessas-MacBook-Pro.local>
2021-05-16 00:20:12 -0700

    adding async js loading

--- functions.php
+++ functions.php
@@ -65,3 +65,12 @@ add_action('wp_enqueue_scripts', '_vive_market_flats_assets', 1000 );
 add_action( 'wp_enqueue_scripts', function(){
     wp_enqueue_script( 'wp-api' );
 } );
+
+// defer js
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
